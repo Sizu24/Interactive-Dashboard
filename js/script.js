3729c151a6159a51121e19a3a2dae174c003a475
Shahetul Chisty <sizu1124@gmail.com>
2020-05-12 13:32:17 -0700

    All chart data complete in js

--- js/script.js
+++ js/script.js
@@ -9,108 +9,112 @@ alertBanner.innerHTML =
 `
 
 alertBanner.addEventListener('click', e =>{
-    const element = e.target;
-    if(element.classList.contains("alert-banner-close")){
-        alertBanner.style.display = "none"
-    }
+  const element = e.target;
+  if(element.classList.contains("alert-banner-close")){
+    alertBanner.style.display = "none"
+  }
 });
 
 /* --------------------------- Charts --------------------------- */
 
 // line chart
 let lineChartId = document.getElementById('myChart');
-var lineChart = new Chart(lineChartId, {
-  type: 'line',
-  data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-      }]
+
+let trafficData = {
+  labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10',
+   '11-17','18-24','25-31'],
+  datasets: [{
+    data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+    backgroundColor: 'rgba(116, 119, 191, 0.3 )',
+    borderwidth: 1,
+  }]
+};
+
+let trafficOptions = {
+  aspectRatio: 2.5,
+  animation: {
+    duration: 0
   },
-  options: {
-      scales: {
-          yAxes: [{
-              ticks: {
-                  beginAtZero: true
-              }
-          }]
-      }
+  scales: {
+    yAxes: [{
+        ticks: {
+            beginAtZero: true
+        }
+    }]
+  },
+  legend: {
+    display: false
   }
+};
+
+let lineChart = new Chart(lineChartId, {
+  type: 'line',
+  data: trafficData,
+  options: trafficOptions
 });
 
 
 // bar chart
 let barChartId = document.getElementById('myBarChart');
+
+let dailyData = {
+  labels: ['S','M','T','W','T','F','S'],
+  datasets: [{
+    label: '# of hits',
+    data: [75, 115, 175, 125, 225, 200, 100],
+    backgroundColor: '#7477BF',
+    borderWidth: 1
+  }]
+};
+
+let dailyOptions = {
+  scales: {
+    yAxes: [{
+      ticks: {
+        beginAtZero: true
+      }
+    }]
+  },
+  legend: {
+    display: false
+  }
+}
+
 let barChart = new Chart(barChartId, {
   type: 'bar',
-  data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-      }]
-  },
-  options: {
-      scales: {
-          yAxes: [{
-              ticks: {
-                  beginAtZero: true
-              }
-          }]
-      }
-  }
+  data: dailyData,
+  options: dailyOptions
 });
 
 // donut chart
 let donutChartId = document.getElementById('myDonutChart');
-Chart.defaults.scale.ticks.beginAtZero = true;
+
+let mobileData = {
+  labels: ['Desktop','Tablet','Phones'],
+  datasets: [{
+    label: '# of Users',
+    data: [2000, 550, 500],
+    borderWidth: 0,
+    backgroundColor: [
+      '#7477BF',
+      '#78CF82',
+      '#51B6C8'
+    ]
+  }]
+};
+
+let mobileOptions = {
+  legend: {
+    position: 'right',
+    labels: {
+      boxWidth: 20,
+      fontStyle: 'bold'
+    }
+  }
+};
+
 let donutChart = new Chart(donutChartId, {
   type: 'doughnut',
-  data: {
-    labels: ['phones', 'tablets', 'desktops'],
-    datasets: [{
-      label: 'Points',
-      backgroundColor: [
-        'rgb(116, 177, 191, 1)',
-        'rgb(129, 201, 143, 1)',
-        'rgb(115, 119, 191, 1)'
-      ],
-      data: [10, 20, 45]
-    }]
-  }
+  data: mobileData,
+  options: mobileOptions
 });
