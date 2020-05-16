bf85375e3005f97abf005c6e0e808a13851504e4
Shahetul Chisty <sizu1124@gmail.com>
2020-05-16 16:21:02 -0700

    Alert Banner fade out

--- js/script.js
+++ js/script.js
@@ -190,6 +190,9 @@ alertBanner.innerHTML =
 alertBanner.addEventListener('click', e =>{
   const element = e.target;
   if(element.classList.contains("alert-banner-close")){
-    alertBanner.style.display = "none"
+    alertBanner.classList.add("inactive");
+    setTimeout(function(){
+      alertBanner.style.display = "none";
+    }, 400);
   }
 });
