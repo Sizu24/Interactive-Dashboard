e77354dc3d134030efd2bb0df9f2a9062b0cdf17
Shahetul Chisty <sizu1124@gmail.com>
2020-05-16 23:32:56 -0700

    Functioning chart with new data

--- js/script.js
+++ js/script.js
@@ -1,18 +1,66 @@
 /* --------------------------- Charts --------------------------- */
 
-// line chart
+// Line chart canvas selctor
 let lineChartId = document.getElementById('myChart');
 
+// Variables
+let activeChart = document.getElementById("traffic-nav");
+let chartList = activeChart.querySelectorAll("li");
+let hourlyChart = ['12-3','3-5','5-8','9-11','11-1','1-3','3-6','7-9','9-11','11-12'];
+let weeklyChart = ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10',
+'11-17','18-24','25-31'];
+let dailyChart = ['S','M','T','W','TH','F','S'];
+let monthlyChart = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+
+// Event listener for switching timeframes for line chart
+activeChart.addEventListener("click", e =>{
+  
+  // Variables
+  let element = e.target;
+
+  // Clean list by removing "active" class name from all items in list
+  for(let i = 0; i < chartList.length; i++){
+    chartList[i].classList.remove("active");
+  }
+
+  // Add "active" class name to clicked item
+  if(element.tagName === "LI"){
+    console.log(element.tagName);
+    element.classList.toggle("active");
+  }
+
+  /* Look for item with "active" class,
+  assign corrct labels to chart, and refresh chart */
+  for(let i = 0; i < chartList.length; i++){
+    if(chartList[i].className === "active"){
+      if(chartList[i].textContent === "Hourly"){
+        trafficData.labels = hourlyChart;
+        lineChart.update();
+      }else if(chartList[i].textContent === "Daily"){
+        trafficData.labels = dailyChart;
+        lineChart.update();
+      }else if(chartList[i].textContent === "Weekly"){
+        trafficData.labels = weeklyChart;
+        lineChart.update();
+      }else{
+        trafficData.labels = monthlyChart;
+        lineChart.update();
+      }
+    }
+  }
+}); // <--- End of event listener function
+
+// Chart Data Object
 let trafficData = {
-  labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10',
-   '11-17','18-24','25-31'],
+  labels: weeklyChart, // labels from above code
   datasets: [{
-    data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+    data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2300, 2500], // data on side of chart
     backgroundColor: 'rgba(116, 119, 191, 0.3 )',
     borderwidth: 1,
   }]
 };
 
+// Chart Options Object
 let trafficOptions = {
   aspectRatio: 2.5,
   animation: {
@@ -30,6 +78,7 @@ let trafficOptions = {
   }
 };
 
+// Chart Ojbect
 let lineChart = new Chart(lineChartId, {
   type: 'line',
   data: trafficData,
@@ -108,9 +157,7 @@ const user = document.getElementById('find-user');
 const message = document.getElementById('text-area');
 const send = document.getElementById('send');
 
-console.log(`Message value is ${message.classList}`);
-console.log(`User value is ${user.value}`);
-
+// Check if search and message fields are empty. Alert user if empty
 send.addEventListener('click', ()=>{
   if(user.value === "" && message.value === ""){
     alert("Both fields musth be filled out to submit");
@@ -123,11 +170,11 @@ send.addEventListener('click', ()=>{
   }
 });
 
-/* ---------------------- Bell notification ---------------------- */
+/* ---------------------- Bell & Notifications ---------------------- */
 
 // variables
 const bell = document.getElementById("bell");
-const greenDot = bell.firstElementChild;
+const blueDot = bell.firstElementChild;
 
 const dropdown = document.getElementById("dropdown");
 
@@ -145,10 +192,11 @@ let listItems =
   <li><span>x</span>${itemThree}</li>
 </ul>
 `
-// Notification Bell & add items
+// Event listener for notification bell & add items to notification list
 bell.addEventListener("click", ()=>{
-  greenDot.style.backgroundColor = "#7377bf";
-  greenDot.style.transitionDuration = "1s";
+  // make blue notification dot disappear into background color
+  blueDot.style.backgroundColor = "#7377bf";
+  blueDot.style.transitionDuration = "1s";
 
   // Adds items to list on bell click, and closes Items
   if(dropdown.innerHTML === ""){
@@ -168,10 +216,11 @@ const closeBox = ()=>{
       let closedropdown = e.target;
       let dropdownItem = closedropdown.parentNode;
       dropdownItem.classList.add("remove");
+
       // Delay funciton to remove list item
       setTimeout(function(){
         dropdownItem.remove();
-      }, 650);
+      }, 450);
     }
   });
 }
@@ -183,10 +232,11 @@ let alertBanner = document.getElementById("alert");
 alertBanner.innerHTML = 
 `
 <div class="alert-banner">
-  <p><strong>Alert: </strong>you have <strong>6 </strong>overdue tasks to complete</p>
+  <p><strong>Alert: </strong>you have <strong>3 </strong>overdue tasks to complete</p>
   <p class="alert-banner-close">x</p>
 </div>
 `
+// Make banner fade out, and add "inactive" class
 alertBanner.addEventListener('click', e =>{
   const element = e.target;
   if(element.classList.contains("alert-banner-close")){
